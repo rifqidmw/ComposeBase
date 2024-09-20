7965182e668a8abfd595f8365493d9da972a6aa3
Diky Arif Febriyanto <dikyariff97@gmail.com>
2024-09-20 16:17:21 +0700

    feat: enhance home view and add categories functionality

--- app/src/main/java/com/aigs/base/ui/screens/home/HomeView.kt
+++ app/src/main/java/com/aigs/base/ui/screens/home/HomeView.kt
@@ -1,31 +1,21 @@
 package com.aigs.base.ui.screens.home
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
@@ -37,21 +27,19 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import coil.compose.AsyncImage
 import com.aigs.base.R
 import com.aigs.base.common.AppConstants.Route
-import com.aigs.base.data.model.Product
-import com.aigs.base.data.model.ProductResponse
 import com.aigs.base.ui.components.BaseTextField
+import com.aigs.base.ui.screens.home.section.HomeAnnouncementSection
+import com.aigs.base.ui.screens.home.section.HomeGreetingsSection
+import com.aigs.base.ui.screens.home.section.HomeProductsSection
 import com.aigs.base.ui.theme.primaryBlack
 import com.aigs.base.ui.theme.primaryBlue
 import com.aigs.base.ui.theme.primaryGray
@@ -85,10 +73,25 @@ fun HomeView(navController: NavController, viewModel: HomeViewModel) {
             onSettingsClicked = { viewModel.onSettingsClicked() },
         )
 
-        when {
-            uiState.isLoading -> CircularProgressIndicator(modifier = Modifier.align(Alignment.CenterHorizontally))
-            uiState.error != null -> Text("Error: ${uiState.error}", color = Color.Red)
-            else -> ProductList(products = uiState.products)
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            contentPadding = PaddingValues(vertical = 16.dp)
+        ) {
+            item {
+                Spacer(modifier = Modifier.height(20.dp))
+                HomeGreetingsSection(name = "Emily")
+                Spacer(modifier = Modifier.height(12.dp))
+                HomeAnnouncementSection()
+                Spacer(modifier = Modifier.height(20.dp))
+                HomeProductsSection(
+                    categories = uiState.categories,
+                    selectedCategory = uiState.selectedCategory,
+                    onCategorySelected = { viewModel.onCategorySelected(it) },
+                    isLoading = uiState.isLoading,
+                    error = uiState.error,
+                    products = uiState.products
+                )
+            }
         }
     }
 }
@@ -102,7 +105,7 @@ fun AppBar(
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(start = 16.dp, end = 16.dp, top = 24.dp),
+            .padding(start = 20.dp, end = 20.dp, top = 24.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
         Text(
@@ -144,101 +147,6 @@ fun AppBar(
                 contentDescription = "Settings",
                 tint = primaryBlue
             )
-        }
-    }
-}
-
-@Composable
-fun ProductList(products: List<Product>) {
-    LazyColumn(
-        modifier = Modifier.fillMaxSize(),
-        contentPadding = PaddingValues(16.dp)
-    ) {
-        items(products) { product ->
-            ProductCard(product)
-            Spacer(modifier = Modifier.height(16.dp))
-        }
-    }
-}
-
-@Composable
-fun ProductCard(product: Product) {
-    Card(
-        colors = CardDefaults.cardColors(Color.Transparent),
-        modifier = Modifier
-            .fillMaxWidth()
-            .height(120.dp),
-    ) {
-        Row(modifier = Modifier.fillMaxSize()) {
-            Card(
-                modifier = Modifier
-                    .size(width = 120.dp, height = 260.dp)
-                    .padding(8.dp),
-                shape = RoundedCornerShape(16.dp),
-                elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
-            ) {
-                AsyncImage(
-                    model = product.images.first(),
-                    contentDescription = null,
-                    modifier = Modifier.fillMaxSize(),
-                    contentScale = ContentScale.Crop
-                )
-            }
-            Box(modifier = Modifier.fillMaxSize()) {
-                Column(
-                    modifier = Modifier
-                        .fillMaxHeight()
-                        .padding(start = 10.dp),
-                    verticalArrangement = Arrangement.Center
-                ) {
-                    Text(
-                        text = product.title,
-                        fontFamily = raleway,
-                        fontWeight = FontWeight.Normal,
-                        fontSize = 12.sp,
-                        lineHeight = 16.sp,
-                        color = primaryBlack,
-                        maxLines = 2,
-                        overflow = TextOverflow.Ellipsis,
-                    )
-                    Spacer(modifier = Modifier.height(12.dp))
-                    Text(
-                        text = "$${product.price}",
-                        fontFamily = raleway,
-                        fontWeight = FontWeight.Bold,
-                        fontSize = 18.sp,
-                        lineHeight = 22.sp,
-                        color = primaryBlack
-                    )
-                    Spacer(modifier = Modifier.height(12.dp))
-                    Box(
-                        modifier = Modifier
-                            .wrapContentSize()
-                            .background(
-                                color = Color(0xFFE5EBFC), shape = RoundedCornerShape(6.dp)
-                            )
-                            .padding(horizontal = 12.dp, vertical = 4.dp)
-                    ) {
-                        Text(
-                            text = product.category,
-                            fontFamily = raleway,
-                            fontWeight = FontWeight.Medium,
-                            fontSize = 14.sp,
-                            lineHeight = 18.sp,
-                            color = primaryBlack
-                        )
-                    }
-                }
-                Image(
-                    painter = painterResource(id = R.drawable.ic_add_cart),
-                    contentDescription = null,
-                    modifier = Modifier
-                        .align(
-                            Alignment.BottomEnd
-                        )
-                        .padding(bottom = 16.dp)
-                )
-            }
         }
     }
 }
