387b7183b5be911142368af0e9a180159ee513bf
Diky Arif Febriyanto <dikyariff97@gmail.com>
2024-09-23 02:52:01 +0000

    Unit Test Example

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -139,6 +139,9 @@ dependencies {
     implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
 
     testImplementation("junit:junit:4.13.2")
+    testImplementation("org.mockito:mockito-core:4.8.0")
+    testImplementation("io.insert-koin:koin-test:3.5.0")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.2")
 
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
