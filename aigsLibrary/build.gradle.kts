7a47dc50c8b85d741433e5857f861056bee5b5e8
rifqidmw <rifqi.dmw@gmail.com>
2024-10-03 09:38:23 +0700

    chore: - enhance build.gradle.kts dependency

--- aigsLibrary/build.gradle.kts
+++ aigsLibrary/build.gradle.kts
@@ -36,8 +36,51 @@ android {
 dependencies {
 
     implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+
+    // Koin
+    implementation(libs.koin.android)
+    implementation(libs.koin.androidx.compose)
+
+    // Retrofit
+    implementation(libs.retrofit)
+    implementation(libs.converter.gson)
+
+    // ViewModel
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+
+    // Accompanist Google Material
+    implementation(libs.accompanist.navigation.material)
+    implementation(libs.accompanist.systemuicontroller)
+
+    // Splash Screen
+    implementation(libs.androidx.core.splashscreen)
+
+    // Coil
+    implementation(libs.coil.compose)
+    implementation(libs.coil.svg)
+
+    // Android SVG
+    implementation(libs.androidsvg.aar)
+
+    // Shared Preferences
+    implementation(libs.androidx.security.crypto)
+
+    // Data Store
+    implementation(libs.androidx.datastore.preferences)
+
+    // HTTP Logging Interceptor
+    implementation(libs.logging.interceptor)
+
+    testImplementation(libs.mockito.core)
+    testImplementation(libs.koin.test)
+    testImplementation(libs.kotlinx.coroutines.test)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
